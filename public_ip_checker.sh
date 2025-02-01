8991953dacd88f3ac41672e4d3d8f61e3cb74585
teraclast <teraclast@gmail.com>
2025-02-01 11:44:37 -0800

    Initial commit of ip tracker script

---
+++ public_ip_checker.sh
@@ -0,0 +1,33 @@
+#!/usr/bin/env bash
+
+# Define the log file
+
+LOGFILE="$HOME/public_ip_log.txt"
+IPV4FILE="$HOME/current_ipv4.txt"
+IPV6FILE="$HOME/current_ipv6.txt"
+
+# Get current IP addrs
+CURRENT_IPV4=$(curl -s ifconfig.me)
+CURRENT_ipV6=$(curl -s -6 ipconfig.co)
+
+# Check and log IPv4 changes
+if [ ! -f "IPV4FILE" ]; then
+    echo "$CURRENT_IPV4" > "$IPV4FILE"
+else
+    PREV_IPV4=$(cat "$IPV4FILE")
+    if [ "$CURRENT_IPV4" != "$PREV_IPV4" ]; then
+        echo "$(date) - IPv4 Changed: $PREV_IPV4 → $CURRENT_IPV4" | tee -a "$LOGFILE" | mail -s "Public IPv4 Changed" terraclast@gmail.com
+        echo "$CURRENT_IPV4" > "IPV4FILE"
+    fi
+fi
+
+# Check and log IPv6  changes
+if [ ! -f "$IPV6FILE" ]; then
+    echo "$CURRENT_IPV6" > "$IPV6FILE"
+else
+    PREV_IPV6=$(cat "$IPV6FILE")
+    if [ "$CURRENT_IPV6" != "$PREV_IPV6" ]; then
+        echo "$(date) - IPv6 Changed: $PREV_IPV6 → $CURRENT_IPV6" | tee -a "$LOGFILE" | mail -s "Public IPv6 Changed" terraclast@gmail.com
+        echo "$CURRENT_IPV6" > "$IPV6FILE"
+    fi
+fi
